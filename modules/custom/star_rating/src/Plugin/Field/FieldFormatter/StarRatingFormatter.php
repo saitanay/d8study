15cdf234d7bd6654db201cd28cdeca8a258c5497
Tanay Sai <saitanay@gmail.com>
2016-04-25 02:29:29 +0530

    Star Rating Module

---
+++ modules/custom/star_rating/src/Plugin/Field/FieldFormatter/StarRatingFormatter.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\star_rating\Plugin\Field\FieldFormatter\StarRatingFormatter.
+ */
+
+namespace Drupal\star_rating\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'star_rating_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "star_rating_formatter",
+ *   label = @Translation("Star rating formatter"),
+ *   field_types = {
+ *     "decimal"
+ *   }
+ * )
+ */
+class StarRatingFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = array(
+        '#theme' => 'star_rating',
+        '#ratingvalue' => $this->viewValue($item) * 20,
+        '#attached' => array('library'=> array('star_rating/star_rating')),
+      );
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
